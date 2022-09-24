2e0feaae9faff13f9fd112421f15c5c66137b5ec
Igor Podgorniy <ipod-92@mail.ru>
2022-09-24 17:41:28 +0300

    Добавить cookie-parser

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const routerUsers = require('./routers/users');
 const routerCards = require('./routers/cards');
 const { NOT_FOUND_ERROR } = require('./constants/errors');
@@ -15,6 +16,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 app.use((req, res, next) => {
   req.user = {
